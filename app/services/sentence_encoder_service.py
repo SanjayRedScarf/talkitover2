7d3b46a63f1a6207255534aef421369b16dab99b
Rana Sandhu <rana.ps.sandhu@gmail.com>
2021-11-10 13:03:26 -0500

    incase user_msg > 250 words the sentence encoder will now split the message as needed

--- app/services/sentence_encoder_service.py
+++ app/services/sentence_encoder_service.py
@@ -39,6 +39,10 @@ class SentenceEncoder:
             return [" ".join(msg[i: j]) for i in range(len(msg)) for j in range(i + 1, len(msg) + 1) if len(msg[i:j]) == msg_size]
         else:
             return [" ".join(msg)]
+    
+    def cut_250_word(self,msg):
+        size = 250
+        return [" ".join(msg[pos:pos+size]) for pos in range(0,len(msg),size)]
 
     def get_cat(self,message):
         all_info = {}
@@ -81,10 +85,14 @@ class SentenceEncoder:
         max_dot_substring_dot =0
         max_dot_substring = ''
         max_dot_exemplar = ''
-        embeded_msg = self.embed(message)
+        if len(message.split())>250:
+            embeded_msg = self.embed(self.cut_250_word(message.split()))
+        else:
+            embeded_msg = self.embed(message)
         for category in self.dataset.keys(): # for every category, taken from aidata json
             dot_products = [] #probably not needed, though maybe for the sentence encoder 250 word limit
             dots = np.inner(embeded_msg,self.cat_embed[category])
+            dots = dots.flatten()
             max_loc = np.argmax(dots)
             dot_products.append(dots[max_loc]) # append max for one exemplar out of all the cut ups of user msgs
             max_dot_per_cat[str(category)] = max(dot_products) # I think there is only one max in here 
@@ -120,6 +128,16 @@ class SentenceEncoder:
 if __name__ == '__main__':
     se = SentenceEncoder()
     se.make_cat_embed()
-    cat = se.get_cat_no_cut('my girlfriend hit me and im being abused and im depressed and also Upset. You fucking suck')['max_over_thresh']
+    test = 'my girlfriend hit me and im being abused and im depressed and also Upset. You fucking suck'
+    # test = '''A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil 
+    # existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray 
+    # gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, 
+    # then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, 
+    # like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God! O my friend'''
+    out = se.get_cat_no_cut(test)
+    cat = out['max_over_thresh']
     print(cat)
     print(se.guse_response(cat,se.repeat))
+    #print(len(se.cut_250_word(test.split())))
+    #print(se.cut_up_msg3(test,'yo yo yo yo'))
+
