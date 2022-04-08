549cce7e50eaa02904dc29287512c360a22b56b6
Ranapss <39842018+Ranapss@users.noreply.github.com>
2022-04-08 11:55:03 -0400

    fix q_heavy naming issue

--- app/repositories/conversation_data_repository.py
+++ app/repositories/conversation_data_repository.py
@@ -36,12 +36,12 @@ class ConversationDataRepository:
                     writer.writeheader()  
                 if user_inputs.ai_data != {}:
                     data = [{'user_id':user_inputs.conversation_id, "user_says": str(user_inputs.message), "chatbot_says": str(user_inputs.response),'version':version, 'frontend':user_inputs.client_id, "campaign": str(google_ads_data.campaign or ''), "group":  str(google_ads_data.group or ''), "geo": str(google_ads_data.geo or ''), "device": str(google_ads_data.device or ''), 
-                    "timestamp":str(datetime.now()),'section':(user_inputs.section) ,'response_type':response_type,'multi':multi,'heavy_q':str(session['heavy_q']),'ai_data':user_inputs.ai_data,
+                    "timestamp":str(datetime.now()),'section':(user_inputs.section) ,'response_type':response_type,'multi':multi,'q_heavy':str(session['heavy_q']),'ai_data':user_inputs.ai_data,
                     'over_threshold':user_inputs.ai_data['max_over_thresh'],'highest_score_category':user_inputs.ai_data['highest_max_score_category'],'highest_score_exemplar':user_inputs.ai_data['exemplar_for_max_cat'],
                     'highest_score_substring':user_inputs.ai_data['substring_for_max_cat']}]
                 else:
                     data = [{'user_id':user_inputs.conversation_id, "user_says": str(user_inputs.message), "chatbot_says": str(user_inputs.response),'version':version,'frontend':user_inputs.client_id, "campaign": str(google_ads_data.campaign or ''), "group":  str(google_ads_data.group or ''), "geo": str(google_ads_data.geo or ''), "device": str(google_ads_data.device or ''),
-                    "timestamp":str(datetime.now()),'section':(user_inputs.section),'response_type':response_type,'multi':multi,'heavy_q':str(session['heavy_q']),'ai_data':user_inputs.ai_data}]
+                    "timestamp":str(datetime.now()),'section':(user_inputs.section),'response_type':response_type,'multi':multi,'q_heavy':str(session['heavy_q']),'ai_data':user_inputs.ai_data}]
                 writer.writerows(data)
 
         return None
